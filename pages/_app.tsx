808485f8e42cecc3b0740887e70fd3b9860b0cf4
sp <sp@kv.media>
2022-08-07 18:31:03 +0300

    delete lib/gtag.ts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,41 +1,22 @@
 import type { AppProps } from 'next/app'
-import { useRouter } from 'next/router'
 import Script from 'next/script'
-import { useEffect } from 'react'
-import * as gtag from '../lib/gtag'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-    const router = useRouter()
-    useEffect(() => {
-      const handleRouteChange = (url) => {
-        gtag.pageview(url)
-      }
-      router.events.on('routeChangeComplete', handleRouteChange)
-      router.events.on('hashChangeComplete', handleRouteChange)
-      return () => {
-        router.events.off('routeChangeComplete', handleRouteChange)
-        router.events.off('hashChangeComplete', handleRouteChange)
-      }
-    }, [router.events])
   return <>
-  <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
+   {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
       <Script
-        id="gtag-init"
+        src="https://www.googletagmanager.com/gtag/js?id=G-HLN5SPK43Y"
         strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
       />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-HLN5SPK43Y');
+        `}
+      </Script>
       <Component {...pageProps} />
   </>
 }
